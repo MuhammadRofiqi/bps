c50ece12174ac683eb047ac9c859f4ebdc52149d
Muhammad Rofiqi <muhammadrofiqi02@gmail.com>
2023-06-28 22:20:10 +0700

    login user

--- app/controllers/usercontrollers.js
+++ app/controllers/usercontrollers.js
@@ -41,4 +41,39 @@ module.exports = class UserController {
       return res.boom.badRequest(error.message);
     }
   }
+
+  static async login(req, res) {
+    try {
+      // check error and retrieve request
+      const { error, value } = login.validate(req.body);
+      if (error) {
+        return res.boom.badData(error.message);
+      }
+
+      // check user
+      const user = await db("users").where({ email: value.email }).first();
+      if (!user) {
+        return res.boom.unauthorized("wrong email, please check again !!");
+      }
+      
+      // check password
+      if (!bcrypt.compareSync(value.password, user.password)) {
+        return res.boom.unauthorized("wrong password, please check again !!");
+      }
+
+      const token = jwt.sign({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      }, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRED_TIME });
+
+      return res.json({
+        success: true,
+        message: "user successfully logged in",
+        token: "Bearer ".concat(token)
+      });
+    } catch (error) {
+      return res.boom.badRequest(error.message);
+    }
+  }
 };
